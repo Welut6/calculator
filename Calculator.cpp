8223164fb7ad18375e5a196ae7ff58d4a7adac38
Welut <Welut.inc@mail.ru>
2025-09-29 23:48:54 +0300

    написан простенький консольный калькулятор

---
+++ Calculator.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() //функция
+{
+	double a, b, res; //переменные 
+	char op; //операция
+
+	cout << "1 number:";
+	cin >> a;
+
+	cout << "operator:";
+	cin >> op;
+
+	cout << "2 number:";
+	cin >> b;
+
+	switch (op) { //switch – выбор действия в зависимости от значения op
+
+	case '+':
+		res = a + b;
+		break;
+	case '-':
+		res = a - b;
+		break;
+	case '*':
+		res = a * b;
+		break;
+	case '/':
+		res = a / b;
+		break;
+	}
+	cout << "resultat=" << res;
+	return 0;
+}
