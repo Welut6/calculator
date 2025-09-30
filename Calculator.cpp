aba18b184f2ac8dcb8d0ca122c1f5d602f3a1e88
Welut <Welut.inc@mail.ru>
2025-09-30 23:10:06 +0300

    добавлен цикл

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,40 +5,52 @@ int main() //функция
 {
 	double a, b, res; //переменные 
 	char op; //операция
+	
+	while (true)
+	{
+		cout << "1 number: ";
+		cin >> a;
 
-	cout << "1 number: ";
-	cin >> a;
+		cout << "operator (only: + - * /): ";
+		cin >> op;
 
-	cout << "operator (only: + - * /): ";
-	cin >> op;
+		cout << "2 number: ";
+		cin >> b;
 
-	cout << "2 number: ";
-	cin >> b;
+		switch (op) { //switch – выбор действия в зависимости от значения op
 
-	switch (op) { //switch – выбор действия в зависимости от значения op
-
-	case '+':
-		res = a + b;
-		break;
-	case '-':
-		res = a - b;
-		break;
-	case '*':
-		res = a * b;
-		break;
-	case '/':
-		if (b == 0) {
-			cout << "You can't divide by zero.";
+		case '+':
+			res = a + b;
+			break;
+		case '-':
+			res = a - b;
+			break;
+		case '*':
+			res = a * b;
+			break;
+		case '/':
+			if (b == 0) {
+				cout << "You can't divide by zero.";
+				return 1;
+			}
+			else {
+				res = a / b;
+			}
+			break;
+		default:
+			cout << "Unknown operation!";
 			return 1;
 		}
-		else {
-			res = a / b;
+		cout << "resultat:\n" << a << " " << op << " " << b << " = " << res;
+
+		bool q;
+		cout << "\nif you want to repeat, press: 1, if not: 0\n";
+		cin >> q;
+		if (q == 0) {
+			return 0;
 		}
-		break;
-	default:
-		cout << "Unknown operation!";
-		return 1;
+		else {
+			
+		}
 	}
-	cout << "resultat:\n" << a << " " << op << " " << b << " = " << res;
-	return 0;
 }
