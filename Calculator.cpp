2479ae3eca109a36de94487283134b8846805958
Welut <Welut.inc@mail.ru>
2025-10-02 22:50:54 +0300

    Добавлена функция возведения в степень ---------------------------------------- Заменили switch на if-else из-за строкового оператора "**" ---------------------------------------- Изменен выход из программы система 0\1 заменена на Y\N

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,59 +1,79 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main() //функция
+int main()
 {
-	double a, b, res; //переменные 
-	char op; //операция
-	
-	while (true)
-	{
-		cout << "1 number: ";
-		cin >> a;
+    double a, b, res;
+    string op; // изменили на string для поддержки "**"
 
-		cout << "operator (only: + - * /): ";
-		cin >> op;
+    while (true)
+    {
+        cout << "1 number: ";
+        cin >> a;
 
-		cout << "2 number: ";
-		cin >> b;
+        cout << "operator (only: +, -, *, /, **): ";
+        cin >> op;
 
-		switch (op) { //switch – выбор действия в зависимости от значения op
+        cout << "2 number: ";
+        cin >> b;
 
-		case '+':
-			res = a + b;
-			break;
-		case '-':
-			res = a - b;
-			break;
-		case '*':
-			res = a * b;
-			break;
-		case '/':
-			if (b == 0) {
-				cout << "You can't divide by zero.";
-				return 1;
-			}
-			else {
-				res = a / b;
-			}
-			break;
-		default:
-			cout << "Unknown operation!";
-			return 1;
-		}
-		cout << "Resultat:\n" << a << " " << op << " " << b << " = " << res << "\n";
+        // Заменяем switch на if-else из-за строкового оператора "**"
+        if (op == "+") {
+            res = a + b;
+        }
+        else if (op == "-") {
+            res = a - b;
+        }
+        else if (op == "*") {
+            res = a * b;
+        }
+        else if (op == "/") {
+            if (b == 0) {
+                cout << "You can't divide by zero.";
+                return 1;
+            }
+            res = a / b;
+        }
+        else if (op == "**") {
+            // логика возведения в степень (только для целых положительных степеней)
+            if (b < 0) {
+                cout << "Error: negative exponent not supported in this implementation";
+                return 1;
+            }
 
-		bool q;
-		cout << "\nIf you want to repeat, press: 1, if not: 0\n";
-		cin >> q;
-		if (q == 0) {
-			break;
-		}
-		else {
-			
-		}
-	}
-	cout << "Goodbye!!!";
-	return 0;
+            double a1 = a;
+            int int_b = (int)b; // преобразуем к целому
+
+            // Проверяем, является ли степень целым числом
+            if (b != int_b) {
+                cout << "Warning: fractional part of exponent will be ignored\n";
+            }
+
+            if (int_b == 0) {
+                res = 1; // любое число в степени 0 = 1
+            }
+            else {
+                for (int i = 1; i < int_b; i++) {
+                    a1 = a1 * a;
+                }
+                res = a1;
+            }
+        }
+        else {
+            cout << "Unknown operation!";
+            return 1;
+        }
+
+        cout << "Result:\n" << a << " " << op << " " << b << " = " << res << "\n";
+
+        char q;
+        cout << "\nYou want to repeat? (Y/N)\n";
+        cin >> q;
+        if (q == 'N' or q == 'n'); {
+            break;
+        }
+    }
+
+    cout << "Goodbye!!!";
+    return 0;
 }
-//tets
